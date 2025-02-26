9c19768c8f9bc1abf72662d36e9ba4eef1118f24
Lee Jongmin <ljm0813b@gmail.com>
2025-02-27 03:16:17 +0900

    Initial commit - Dearfam Spring Boot setup

---
+++ src/main/java/com/example/dearfam/DearfamApplication.java
@@ -0,0 +1,13 @@
+package com.example.dearfam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearfamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearfamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dearfam/DearfamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dearfam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearfamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
