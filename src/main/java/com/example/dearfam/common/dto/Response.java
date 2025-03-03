d9a5417548e1652bc1945e1c6b1f0e70b921c1fb
Lee Jongmin <ljm0813b@gmail.com>
2025-03-04 01:50:42 +0900

    [feat] Generic Response 클래스 설정

---
+++ src/main/java/com/example/dearfam/common/dto/Response.java
@@ -0,0 +1,20 @@
+package com.example.dearfam.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Response<T> {
+    private int code; // 응답 코드
+    private String message; // 응답 메시지
+    private T data; // 제네릭 타입의 응답 데이터
+
+    public static <T> Response<T> of(int code, String message, T data) {
+        return new Response<>(code, message, data);
+    }
+
+    public static <T> Response<T> data(T data) {
+        return new Response<>(0, "", data);
+    }
+}
