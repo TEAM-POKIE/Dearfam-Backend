4512709509cfb0c2c2e02b1c4c68ac644a97ce3f
Lee Jongmin <ljm0813b@gmail.com>
2025-03-07 16:00:29 +0900

    [feat] jwt 인증 Security Confg 적용
    jwt 인증을 사용하기 때문에, Session 인증 방식은 Disable 하였습니다.

--- src/main/java/com/example/dearfam/common/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/example/dearfam/common/configuration/security/SecurityConfiguration.java
@@ -1,36 +1,52 @@
 package com.example.dearfam.common.configuration.security;
 
+import com.example.dearfam.common.jwt.filter.JwtFilter;
+import com.example.dearfam.common.jwt.handler.JwtAccessDeniedHandler;
+import com.example.dearfam.common.jwt.handler.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtFilter jwtFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//                .headers(headers ->
-//                        headers.addHeaderWriter(new XFrameOptionsHeaderWriter(
-//                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)
-//                        )
-//                );
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .cors(cors -> {})
+
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                         .accessDeniedHandler(jwtAccessDeniedHandler)
                 )
 
                 .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
 
                 .authorizeHttpRequests(
                         authorize -> authorize
                                 .requestMatchers(request -> request.getRequestURI().startsWith("/swagger-ui")).permitAll()
                                 .requestMatchers(request -> request.getRequestURI().startsWith("/v3/api-docs")).permitAll()
-                                .requestMatchers(request -> request.getRequestURI().startsWith("/dev/ping")).permitAll()
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/dev/**")).authenticated()
                                 .requestMatchers(request -> request.getRequestURI().startsWith("/h2-console")).permitAll()
                                 .anyRequest().authenticated()
                 )
