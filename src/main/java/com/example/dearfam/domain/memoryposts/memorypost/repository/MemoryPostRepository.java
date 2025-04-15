3479073867396d26ca6fe96f67d2f3ebfc2e7570
Lee Jongmin <ljm0813b@gmail.com>
2025-04-15 14:17:33 +0900

    [chore] repository 어노테이션 수정

--- src/main/java/com/example/dearfam/domain/memoryposts/memorypost/repository/MemoryPostRepository.java
+++ src/main/java/com/example/dearfam/domain/memoryposts/memorypost/repository/MemoryPostRepository.java
@@ -2,6 +2,8 @@ package com.example.dearfam.domain.memoryposts.memorypost.repository;
 
 import com.example.dearfam.domain.memoryposts.memorypost.entity.MemoryPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemoryPostRepository extends JpaRepository<MemoryPost, Long> {
 }
