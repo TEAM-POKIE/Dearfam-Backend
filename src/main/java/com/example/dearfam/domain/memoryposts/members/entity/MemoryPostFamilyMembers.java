3f9ff094f93a3f5fe07375963210b940d5375b4c
Lee Jongmin <ljm0813b@gmail.com>
2025-04-16 10:17:51 +0900

    [chore] nullable false 명시

--- src/main/java/com/example/dearfam/domain/memoryposts/members/entity/MemoryPostFamilyMembers.java
+++ src/main/java/com/example/dearfam/domain/memoryposts/members/entity/MemoryPostFamilyMembers.java
@@ -18,6 +18,7 @@ public class MemoryPostFamilyMembers extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
