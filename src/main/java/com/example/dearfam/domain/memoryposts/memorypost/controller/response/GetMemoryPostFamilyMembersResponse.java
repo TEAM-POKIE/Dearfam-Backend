477e75dda87cfb6fca8431d56f76f1355c182638
Lee Jongmin <ljm0813b@gmail.com>
2025-04-17 12:25:41 +0900

    [chore] Response Dto에 게시글 작성자의 id를 넣기 위해 수정

--- src/main/java/com/example/dearfam/domain/memoryposts/memorypost/controller/response/GetMemoryPostFamilyMembersResponse.java
+++ src/main/java/com/example/dearfam/domain/memoryposts/memorypost/controller/response/GetMemoryPostFamilyMembersResponse.java
@@ -1,5 +1,6 @@
 package com.example.dearfam.domain.memoryposts.memorypost.controller.response;
 
+import com.example.dearfam.domain.memoryposts.memorypost.entity.MemoryPost;
 import com.example.dearfam.domain.users.dto.FamilyMemberDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,11 +15,13 @@ import static lombok.AccessLevel.PRIVATE;
 @Builder(access = PRIVATE)
 public class GetMemoryPostFamilyMembersResponse {
     private Long postId;
+    private Long writerId;
     private List<FamilyMemberDto> participants;
 
-    public static GetMemoryPostFamilyMembersResponse from(Long postId, List<FamilyMemberDto> participants) {
+    public static GetMemoryPostFamilyMembersResponse from(MemoryPost memoryPost, List<FamilyMemberDto> participants) {
         return GetMemoryPostFamilyMembersResponse.builder()
-                .postId(postId)
+                .postId(memoryPost.getId())
+                .writerId(memoryPost.getWriter().getId())
                 .participants(participants)
                 .build();
     }
