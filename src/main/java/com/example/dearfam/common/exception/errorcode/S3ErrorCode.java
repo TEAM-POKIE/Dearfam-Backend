07903aac0271bded07ed5ccc4ee0ec2a73ea560b
Lee Jongmin <ljm0813b@gmail.com>
2025-07-10 14:39:41 +0900

    [feat] S3 Service 클래스 구현 & S3 에러 코드 추가

--- src/main/java/com/example/dearfam/common/exception/errorcode/S3ErrorCode.java
+++ src/main/java/com/example/dearfam/common/exception/errorcode/S3ErrorCode.java
@@ -11,7 +11,8 @@ public enum S3ErrorCode implements ErrorCode{
     INVALID_IMAGE_MIME("이미지 MIME 타입이 아닙니다", HttpStatus.UNSUPPORTED_MEDIA_TYPE),
     UNSUPPORTED_EXTENSION("지원하지 않는 확장자입니다", HttpStatus.BAD_REQUEST),
     MISSING_EXTENSION("확장자가 없습니다", HttpStatus.BAD_REQUEST),
-    UPLOAD_FAILED("S3 업로드에 실패했습니다", HttpStatus.INTERNAL_SERVER_ERROR);
+    UPLOAD_FAILED("S3 업로드에 실패했습니다", HttpStatus.INTERNAL_SERVER_ERROR),
+    IMAGE_LIMIT_EXCEEDED("이미지는 최대 10개까지 업로드 가능합니다", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final HttpStatus httpStatus;
---
+++ src/main/java/com/example/dearfam/common/service/S3Service.java
@@ -0,0 +1,99 @@
+package com.example.dearfam.common.service;
+
+import com.example.dearfam.common.exception.errorcode.S3ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private static final Set<String> ALLOWED_EXTENSIONS = Set.of("jpg", "jpeg", "png");
+
+    public String upload(MultipartFile file, Long postId) {
+        //1. 파일 유효성 검사하기
+        validateImageFile(file);
+
+        // 2. S3에 저장될 파일 경로 생성 (e.g., posts/1/uuid.jpg)
+        String extension = getExtension(file);
+        String key = generateKey(postId, extension);
+
+        // 3. S3 업로드 요청 객체 생성
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(file.getContentType())
+                .contentLength(file.getSize())
+                .build();
+        try {
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+            log.info("파일 업로드 성공 - postId: {}, fileName: {}, key: {}", postId, file.getOriginalFilename(), key);
+        } catch (IOException e) {
+            log.error("파일을 읽어오는 중 오류가 발생했습니다.", e);
+            throw S3ErrorCode.UPLOAD_FAILED.defaultException(e);
+        } catch (SdkException e) {
+            log.error("S3 업로드에 실패했습니다.", e);
+            throw S3ErrorCode.UPLOAD_FAILED.defaultException(e);
+        }
+
+        return key;
+    }
+
+    public String generateUrlFromKey(String key) {
+        GetUrlRequest getUrlRequest = GetUrlRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+        return s3Client.utilities().getUrl(getUrlRequest).toExternalForm();
+    }
+
+    private void validateImageFile(MultipartFile file) {
+        // 파일이 비어있을 때
+        if (file.isEmpty()) {
+            throw S3ErrorCode.EMPTY_FILE.defaultException();
+        }
+        // TODO: 파일이 image 검사 X multipart/form-data 인지를 검사
+        // 파일이 이미지가 아닐때 (Content Type이 image로 시작하지 않는 경우)
+//        if (!Objects.requireNonNull(file.getContentType()).startsWith("image/")) {
+//            log.error(file.getContentType());
+//            throw S3ErrorCode.INVALID_IMAGE_MIME.defaultException();
+//        }
+        // 지원하지 않는 확장자인 경우
+        if (!ALLOWED_EXTENSIONS.contains(getExtension(file))) {
+            throw S3ErrorCode.UNSUPPORTED_EXTENSION.defaultException();
+        }
+    }
+
+    // 파일 확장자 추출하는 함수
+    private String getExtension(MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        if (fileName == null || !Objects.requireNonNull(fileName).contains(".")) {
+            throw S3ErrorCode.MISSING_EXTENSION.defaultException();
+        }
+        return fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+    }
+
+    private String generateKey(Long postId, String extension) {
+        return "posts/" + postId + "/" + UUID.randomUUID() + "." + extension;
+    }
+
+}
