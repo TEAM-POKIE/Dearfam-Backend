64118487619f1e7d30f677f1b55b7bc0e1a2bbcb
Lee Jongmin <ljm0813b@gmail.com>
2025-07-13 17:50:26 +0900

    [feat] 댓글 생성 시 반환 값에 이미지 로직 추가
    MemoryPostMapper 클래스를 통해 이미지 검사 후 dto 생성 및 반환

--- src/main/java/com/example/dearfam/domain/memoryposts/comment/controller/response/GetCreatedCommentResponse.java
+++ src/main/java/com/example/dearfam/domain/memoryposts/comment/controller/response/GetCreatedCommentResponse.java
@@ -1,28 +1,17 @@
 package com.example.dearfam.domain.memoryposts.comment.controller.response;
 
-import com.example.dearfam.domain.memoryposts.comment.dto.MemoryPostCommentDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import static lombok.AccessLevel.PRIVATE;
-
 @Getter
 @AllArgsConstructor
-@Builder(access = PRIVATE)
+@Builder
 public class GetCreatedCommentResponse {
     private Long postId;
+    private String content;
     private Long commentWriterId;
     private String commentWriterName;
-    private String content;
-    // TODO : 이미지 처리 해야함
+    private String commentWriterProfileImage;
 
-    public static GetCreatedCommentResponse from(MemoryPostCommentDto memoryPostCommentDto) {
-        return GetCreatedCommentResponse.builder()
-                .postId(memoryPostCommentDto.getMemoryPost().getId())
-                .commentWriterId(memoryPostCommentDto.getCommentWriter().getId())
-                .commentWriterName(memoryPostCommentDto.getCommentWriter().getUserNickname())
-                .content(memoryPostCommentDto.getCommentContent())
-                .build();
-    }
 }
