160c4cfc14aa29267b6929752e5afaa26b43aa55
Lee Jongmin <ljm0813b@gmail.com>
2025-07-27 16:13:31 +0900

    [chore] @Repository 어노테이션 제거
    현재 Spring Data JPA 의 인터페이스형 Repository로 구현해, 어노테이션 붙이지 않아도 작동함.

--- src/main/java/com/example/dearfam/domain/memoryposts/like/repository/MemoryPostLikeRepository.java
+++ src/main/java/com/example/dearfam/domain/memoryposts/like/repository/MemoryPostLikeRepository.java
@@ -4,9 +4,7 @@ import com.example.dearfam.domain.memoryposts.like.entity.MemoryPostLike;
 import com.example.dearfam.domain.memoryposts.memorypost.entity.MemoryPost;
 import com.example.dearfam.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemoryPostLikeRepository extends JpaRepository<MemoryPostLike, Long> {
     MemoryPostLike findByLikedUserAndMemoryPost(Users likedUser, MemoryPost memoryPost);
     boolean existsByLikedUserAndMemoryPost(Users likedUser, MemoryPost memoryPost);
--- src/main/java/com/example/dearfam/domain/memoryposts/members/repository/MemoryPostFamilyMembersRepository.java
+++ src/main/java/com/example/dearfam/domain/memoryposts/members/repository/MemoryPostFamilyMembersRepository.java
@@ -3,11 +3,9 @@ package com.example.dearfam.domain.memoryposts.members.repository;
 import com.example.dearfam.domain.memoryposts.members.entity.MemoryPostFamilyMembers;
 import com.example.dearfam.domain.memoryposts.memorypost.entity.MemoryPost;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface MemoryPostFamilyMembersRepository extends JpaRepository<MemoryPostFamilyMembers, Long> {
     List<MemoryPostFamilyMembers> findAllByMemoryPost(MemoryPost memoryPost);
 }
