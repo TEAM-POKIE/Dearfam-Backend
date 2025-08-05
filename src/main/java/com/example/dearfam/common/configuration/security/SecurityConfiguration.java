23cc7586e1c34fac14e4ceb5f989a1742174e6ed
Lee Jongmin <ljm0813b@gmail.com>
2025-08-05 17:09:21 +0900

    [chore] 배포 주소 올리기

--- src/main/java/com/example/dearfam/common/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/example/dearfam/common/configuration/security/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(
                 "https://dearfam-front-end.vercel.app",
-                "https://dev.dearfam.store",
+                "https://api.dearfam.store",
                 "http://localhost:8080",
                 "http://10.10.2.179:8080/"
         ));
