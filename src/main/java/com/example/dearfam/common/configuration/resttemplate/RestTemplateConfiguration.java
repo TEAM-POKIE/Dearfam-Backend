37dbf19421965e1c73f1f0c156b09188be7e1246
Lee Jongmin <ljm0813b@gmail.com>
2025-08-07 12:40:59 +0900

    Revert "[chore] 타임아웃 더 길게 설정"
    This reverts commit 06fe0695d419a526124c7ea869ec0924bc36129b.

--- src/main/java/com/example/dearfam/common/configuration/resttemplate/RestTemplateConfiguration.java
+++ src/main/java/com/example/dearfam/common/configuration/resttemplate/RestTemplateConfiguration.java
@@ -2,6 +2,7 @@ package com.example.dearfam.common.configuration.resttemplate;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,9 @@ public class RestTemplateConfiguration {
     // RestTemplate을 통해 소셜 API와 통신하기 위해 구현
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(10_000); // 연결 타임아웃 (10초)
+        factory.setReadTimeout(90_000);   // 응답 타임아웃 (90초)
+        return new RestTemplate(factory);
     }
 }
