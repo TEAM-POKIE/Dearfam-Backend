25c670fa8c30c73ddb35dc3319d8b0b624fe2992
Lee Jongmin <ljm0813b@gmail.com>
2025-08-07 14:13:02 +0900

    [chore] 타임아웃 재설정

--- src/main/java/com/example/dearfam/common/configuration/resttemplate/RestTemplateConfiguration.java
+++ src/main/java/com/example/dearfam/common/configuration/resttemplate/RestTemplateConfiguration.java
@@ -7,12 +7,14 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfiguration {
-    // RestTemplate을 통해 소셜 API와 통신하기 위해 구현
+
     @Bean
     public RestTemplate restTemplate() {
+        // 타임아웃 설정만 적용
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
         factory.setConnectTimeout(10_000); // 연결 타임아웃 (10초)
-        factory.setReadTimeout(90_000);   // 응답 타임아웃 (90초)
+        factory.setReadTimeout(90_000);    // 응답 타임아웃 (90초)
+
         return new RestTemplate(factory);
     }
 }
